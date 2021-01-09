288c256e385efaf2131159fda140065f4d88eef6
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-01-10 06:02:49 +0900

    ENH main화면 Viewpager로 구현

--- app/src/main/java/with/dee2/kotilnproject/MainActivity.kt
+++ app/src/main/java/with/dee2/kotilnproject/MainActivity.kt
@@ -1,42 +1,67 @@
 package with.dee2.kotilnproject
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 
-class MainActivity : AppCompatActivity() {
+
+private const val NUM_PAGES = 3
+
+class MainActivity : FragmentActivity() {
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setFlag(0)
-        btn_friend.setOnClickListener{
-            setFlag(0)
-        }
-        btn_questions.setOnClickListener{
-            setFlag(1)
-        }
-        btn_board.setOnClickListener{
-            setFlag(2)
-        }
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.pager)
 
-  }
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
 
-    private fun setFlag(fragNum : Int) {
-        val ft =supportFragmentManager.beginTransaction()
-        when(fragNum)
-        {
-            0 -> {
-                ft.replace(R.id.main_frame, FriendListFragment()).commit()
-            }
-            1 -> {
-                ft.replace(R.id.main_frame, QuestionListFragment()).commit()
-            }
-            2 -> {
-                ft.replace(R.id.main_frame, BoardFragment()).commit()
-            }
+        var tabList=arrayListOf(
+            "친구","질문","게시판"
+        )
+        TabLayoutMediator(findViewById(R.id.tabLayout), viewPager){tab,position->
+            tab.text=tabList[position]
+        }.attach()
 
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 ->  FriendListFragment()
+                1 ->  QuestionListFragment()
+                else -> BoardFragment()
+            }
         }
 
     }
