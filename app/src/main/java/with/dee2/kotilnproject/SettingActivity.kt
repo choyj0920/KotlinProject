bc818f9de974c4b06380ab2aa437afd5fdbd9702
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-01-13 01:48:03 +0900

    merge_hayoung 오류수정

--- app/src/main/java/with/dee2/kotilnproject/SettingActivity.kt
+++ app/src/main/java/with/dee2/kotilnproject/SettingActivity.kt
@@ -1,12 +1,13 @@
 package with.dee2.kotilnproject
 
 import android.content.Intent
-import android.net.Uri
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_setting.*
+import java.net.URL
 
 class SettingActivity : AppCompatActivity() {
 
