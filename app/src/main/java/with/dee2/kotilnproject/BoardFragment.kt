e730b1b5e4b39ff1a242e17547ae051ce04d1887
hayoung2 <gkdud1308@naver.com>
2021-01-12 20:51:48 +0900

    ENH: 답변 익명 처리 구현

--- app/src/main/java/with/dee2/kotilnproject/BoardFragment.kt
+++ app/src/main/java/with/dee2/kotilnproject/BoardFragment.kt
@@ -52,21 +52,28 @@ class BoardFragment:Fragment() {
                         content=map["content"].toString()
                         question=map["question"].toString()
                     }
-                    ref.child("users").child(uid)
-                        .addValueEventListener(object : ValueEventListener{
-                            override fun onCancelled(error: DatabaseError) {
+                    if (uid!="private"){
+                        ref.child("users").child(uid)
+                            .addValueEventListener(object : ValueEventListener{
+                                override fun onCancelled(error: DatabaseError) {
 
-                            }
+                                }
 
-                            override fun onDataChange(snapshot: DataSnapshot) {
-                                var map =snapshot.value as Map<String,Any>
-                                var name =map["name"].toString()
-                                var img=map["imageUrl"].toString()
-                                dataList.add(Board(img,name,question,content,date))
-                                rvBoard.adapter=BoardAdapter(requireContext(),dataList)
-                            }
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    var map =snapshot.value as Map<String,Any>
+                                    var name =map["name"].toString()
+                                    var img=map["imageUrl"].toString()
+                                    dataList.add(Board(img,"By "+name,question,content,date))
+                                    rvBoard.adapter=BoardAdapter(requireContext(),dataList)
+                                }
 
-                        })
+                            })
+                    }else{
+                        val imgUri="https://firebasestorage.googleapis.com/v0/b/kotilnproject-8e7fe.appspot.com/o/images%2Fvector-creator%20(1).png?alt=media&token=a4d0b071-7e00-4f87-886b-58c2232b258c"
+                        dataList.add(Board(imgUri,"By 익명",question,content,date))
+                        rvBoard.adapter=BoardAdapter(requireContext(),dataList)
+                    }
+
                 }
 
             }
